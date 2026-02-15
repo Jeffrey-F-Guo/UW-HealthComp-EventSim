5b00da9a95a978bcbd48750c43964ef08f20c110
Jeffrey-F-Guo <jeffrey.guo00703@gmail.com>
2026-02-15 14:46:53 -0800

    Created queue data struct for prod con

---
+++ Queue.c
@@ -0,0 +1,60 @@
+#include "Queue.h"
+
+int enqueue(Queue *q, MedicaidPacket packet) {
+    pthread_mutex_lock(&q->lock);
+    if (q->count >= Q_SIZE) {
+        pthread_mutex_unlock(&q->lock);
+        return -1;
+    }
+    q->count++;
+    q->packets[q->head] = packet;
+    q->head = (q->head + 1) % Q_SIZE;
+
+    pthread_cond_signal(&q->not_empty);
+    pthread_mutex_unlock(&q->lock);
+    return 0;
+
+}
+
+MedicaidPacket dequeue(Queue *q) {
+    MedicaidPacket packet = {0};
+    pthread_mutex_lock(&q->lock);
+    while (q->count == 0) {
+        pthread_cond_wait(&q->not_empty, &q->lock);
+    }
+
+    q->count--;
+    packet = q->packets[q->tail];
+    q->tail = (q->tail + 1) % Q_SIZE;
+
+    pthread_mutex_unlock(&q->lock);
+    return packet;
+}
+
+int init_queue(Queue *q) {
+    if (!q) {
+        return -1;
+    }
+    memset(q, 0, sizeof(Queue));
+    q->packets = (MedicaidPacket *)malloc(sizeof(MedicaidPacket) * Q_SIZE);
+    if (!q->packets) {
+        return -1;
+    }
+    q->queue_size = Q_SIZE;
+    q->head = 0;
+    q->tail = 0;
+    q->count = 0;
+    pthread_mutex_init(&q->lock, NULL);
+    pthread_cond_init(&q->not_empty, NULL);
+    return 0;
+}
+
+int cleanup_queue(Queue *q) {
+    if (q && q->packets) {
+        free(q->packets);
+        q->packets = NULL;
+    }
+    pthread_mutex_destroy(&q->lock);
+    pthread_cond_destroy(&q->not_empty);
+    return 0;
+}
---
+++ Queue.h
@@ -0,0 +1,21 @@
+# include "Medicaid.h"
+#include <pthread.h>
+#include <stdlib.h>
+#define Q_SIZE 10000
+
+
+typedef struct {
+    MedicaidPacket *packets;
+    int queue_size;
+    int head;
+    int tail;
+    int count;
+
+    pthread_mutex_t lock;
+    pthread_cond_t not_empty; // condition variable to signal worker threads
+} Queue;
+
+int enqueue(Queue *q, MedicaidPacket packet);
+MedicaidPacket dequeue(Queue *q);
+int init_queue(Queue *q);
+int cleanup_queue(Queue *q);
